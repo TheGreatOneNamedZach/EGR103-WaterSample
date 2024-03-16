3e7b703f42fc791247ab192643985982584a7c8e
TheGreatOneNamedZach <thegreatonenamedzach@gmail.com>
2024-03-15 21:48:37 -0400

    Started GUI Build of Concept

---
+++ other/GUI_ProofOfConcept/GUI_Build.m
@@ -0,0 +1,33 @@
+% The primary parent script for the water sample project.
+close all;
+clear;
+clc;
+
+%a = arduino(); % Initializes the Arduino
+camList = string(webcamlist); % Creates a list of all valid cameras
+camList(2) = "Dummy Webcam";
+
+webcamNum = 99; % The webcam index number choosen by the user. Dummy value of 99
+% Loops until the user chooses a valid webcam
+while (webcamNum > length(camList)) || (webcamNum < 0)
+    % These 4 lines are rerun to update the list of webcams
+    clear;
+    clc;
+    camList = string(webcamlist);
+    % camList(2) = "Dummy Webcam";
+
+    fprintf("Please select a webcam:\n");
+    
+    % Displays a list of all webcams to terminal
+    for i=1 : length(camList)
+        fprintf("%2.0f.) %s\n", i, camList(i));
+    end
+    
+    webcamNum = floor(input("Select the webcam by entering it's number: "));
+end
+
+cam = webcam(camList(webcamNum)); % Constructs a webcam object
+
+ranFromWaterSamplerScript = true; % Verifies app was ran from script
+
+waterSamplerGUI; % Opens the app
